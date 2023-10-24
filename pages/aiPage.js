5af06dcee089a702e0a80fdc89b5da01dda16490
Alex <148038194+Einfachalf@users.noreply.github.com>
2023-10-24 15:14:40 +0200

    Update aiPage.js

--- pages/aiPage.js
+++ pages/aiPage.js
@@ -1,16 +1,10 @@
-
 import { useEffect, useState } from 'react';
-import { loadModels } from 'replicate';
 import { pipeline } from '@huggingface/transformers';
 
-// Hier könnten wir die Modelle von Replicate und Huggingface initialisieren
 const initializeModels = async () => {
-  // Beispiel für das Laden eines Modells mit Replicate
-  const model = await loadModels('dein_model_name');
-
-  // Beispiel für das Initialisieren eines Pipelines mit Huggingface
-  const nlp = pipeline('sentiment-analysis');
-  return { model, nlp };
+  // Initialisiere das Huggingface-Modell
+  const nlp = pipeline('text-generation', model='mistralai/Mistral-7B-Instruct-v0.1');
+  return { nlp };
 };
 
 const AiPage = () => {
@@ -30,3 +24,4 @@ const AiPage = () => {
 };
 
 export default AiPage;
+
